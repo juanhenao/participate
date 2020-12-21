6c3fa1ea95c4c4653eed3ac50964fc6c55b28bfc
Juan Henao <juanhenao10@gmail.com>
2020-12-21 22:14:45 +0100

    Some styling

--- public/index.php
+++ public/index.php
@@ -15,33 +15,37 @@ $students = $controller->index();
 <head>
     <meta charset="UTF-8">
     <title>Participate</title>
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
 </head>
 <body>
-    <h1>Student List</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>id</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Inscription</th>
-                <th>Contributions</th>
-            </tr>
-        </thead>
-        <tbody>
-        <?php
-        foreach ($students as $student) {
-            echo "<tr>";
-            echo "<td>{$student->getId()}</td>";
-            echo "<td>{$student->getFullName()}</td>";
-            echo "<td>{$student->getEmail()}</td>";
-            echo "<td>{$student->getCreatedAt()->format('d.m.Y')}</td>";
-            echo "<td>*</td>";
-            echo "</tr>";
-        }
-        ?>
-        </tbody>
-    </table>
-    <a href="student/add.php">Add a new student</a>
+    <div class="container-md">
+        <h1>Student List</h1>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Inscription</th>
+                    <th>Contributions</th>
+                </tr>
+            </thead>
+            <tbody>
+            <?php
+            foreach ($students as $student) {
+                echo "<tr>";
+                echo "<td>{$student->getId()}</td>";
+                echo "<td>{$student->getFullName()}</td>";
+                echo "<td>{$student->getEmail()}</td>";
+                echo "<td>{$student->getCreatedAt()->format('d.m.Y')}</td>";
+                echo "<td>*</td>";
+                echo "</tr>";
+            }
+            ?>
+            </tbody>
+        </table>
+        <a href="student/add.php" class="btn btn-primary">Add a new student</a>
+    </div>
 </body>
 </html>
--- public/student/add.php
+++ public/student/add.php
@@ -5,7 +5,6 @@ use App\Repositories\Student as StudentRepository;
 use App\Controllers\StudentController;
 use App\Models\Student;
 
-var_dump($_POST);
 if(isset($_POST['submit'])){
 
     $controller = new StudentController(new StudentRepository());
@@ -28,18 +27,27 @@ if(isset($_POST['submit'])){
 <head>
     <meta charset="UTF-8">
     <title>Add Student</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
 </head>
 <body>
-    <h1>Add new student</h1>
-    <form method="post">
-        <label for="name">First name:</label>
-        <input type="text" id="name" name="name">
-        <label for="last_name">Last name:</label>
-        <input type="text" id="last_name" name="last_name">
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email">
-        <input type="submit" name="submit" value="Create">
-    </form>
-
+    <div class="container-md">
+        <h1>Add new student</h1>
+        <form method="post">
+            <div class="mb-3">
+                <label for="name" class="form-label">First name:</label>
+                <input type="text" class="form-control" id="name" name="name">
+            </div>
+            <div class="mb-3">
+                <label for="last_name" class="form-label">Last name:</label>
+                <input type="text" class="form-control" id="last_name" name="last_name">
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">Email:</label>
+                <input type="text" class="form-control" id="email" name="email">
+            </div>
+            <input type="submit" class="btn btn-primary" name="submit" value="Create">
+        </form>
+    </div>>
 </body>
 </html>
